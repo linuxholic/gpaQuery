3667cd4d0693a478d960b6f0644f1ba6564c0fd3
newbie <594424877@qq.com>
2014-07-10 14:43:27 +0800

    output the course name in Chinese

--- gpa.py
+++ gpa.py
@@ -4,23 +4,43 @@ import urllib2
 import re
 import sys
 
-subjects = ['0821101','0821102','0121019','0241001',
-			'0341001','0341002','0341003','0341022',
-			'0721001','0241006','0341006','0341007',
-			'0341008','0341009','0341011','0341012',
-			'0341013','0341014','0341015','0341016',
-			'0341017','0341018','0341020','0341024',
-			'0341029','0342209','0821005']
+subjects = [('0821101','中国特色'),
+			('0821102','自然辩证法概论'),
+			('0121019','有限域及其应用'),
+			('0241001','矩阵论'),
+			('0341001','计算机科学使用的数理逻辑'),
+			('0341002','组合数学'),
+			('0341003','数论算法'),
+			('0341022','工程优化算法'),
+			('0721001','随机过程'),
+			('0241006','数字信号处理(二)'),
+			('0341006','计算机网络(二)'),
+			('0341007','人工智能'),
+			('0341008','模式识别'),
+			('0341009','数字图像处理'),
+			('0341011','面向对象技术'),
+			('0341012','算法分析与设计'),
+			('0341013','分布处理系统'),
+			('0341014','形式语言与自动机'),
+			('0341015','网络安全'),
+			('0341016','计算机系统结构(二)'),
+			('0341017','计算机图形学'),
+			('0341018','计算机控制'),
+			('0341020','基于互联网的计算'),
+			('0341024','软件体系结构'),
+			('0341029','人工神经网络基础'),
+			('0342209','软件工程'),
+			('0821005','英语学位考试')]
 
 def production(series, subject):
-	targtUrl = 'http://210.27.12.1:90/queryDegreeScoreAction.do?studentid=xdleess20130621zq%s&degreecourseno=%s'%(series,subject)
+	targtUrl = 'http://210.27.12.1:90/queryDegreeScoreAction.do?studentid=xdleess20130621zq%s&degreecourseno=%s'%(series,subject[0])
 	content = urllib2.urlopen(targtUrl).read()
 	match = re.findall('<td width="7%" align="left">(.+?)</td>', content, re.S)
 	if match:
 		num = match[1].strip()
 		if len(num) == 0:
 			return (0, 0, False)
-		print subject + ' -> ' + num
+		print subject[1] + '\t(%s)'%subject[0] + '\t-> ' + num
 		score = float(match[1].strip())
 		credit = float(match[2].strip())
 		return (score*credit, credit, True)
@@ -30,6 +50,7 @@ def production(series, subject):
 def average(series):
 	scores = 0
 	credits = 0
+	print
 	for i in range(len(subjects)):
 		(midscores,credit,exist) = production(series, subjects[i])
 		if exist:
@@ -39,7 +60,9 @@ def average(series):
 		ave = scores / credits
 	else:
 		ave = 0
-	print 'Your average score: %.2f'%ave
+	print
+	print '\tYour average score: %.2f'%ave
+	print
 
 def usage():
 	print
@@ -51,11 +74,11 @@ def usage():
 def main(argv):
 	length = len(subjects)
 	for i in range(length):
-		if subjects[i][2] == '2':
+		if subjects[i][0][2] == '2':
 			continue
 		else:
-			apd=subjects[i][0:2] + '2' + subjects[i][3:]
-		subjects.append(apd)
+			apd=subjects[i][0][0:2] + '2' + subjects[i][0][3:]
+			subjects.append((apd, subjects[i][1]))
 
 	if not argv:
 		usage()
